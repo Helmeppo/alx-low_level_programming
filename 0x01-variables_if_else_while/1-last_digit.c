891e9fb6b6a3dcfbb16e9cf7b1cad6772da23500
Helmeppo <gamerblliissard@gmail.com>
2022-10-16 06:12:11 -0700

    some chages for better results Experiment 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,15 +10,22 @@
 int main(void)
 {
 	int n;
+	int ldigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-		printf("Last digit of %d is %d ", n, n % 10);
-	if (n % 10 > 5)
-		printf("and is greater than 5\n");
-	else if (n % 10 == 0)
-       		printf("and is 0\n");
-	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 and not 0\n");
-	return (0);
+	ldigit = n % 10;
+	if (ldigit > 5)
+		{	
+			printf("Last digit of %d is %d and is greater than 5\n", n, ldigit);
+				}
+	else if (ldigit == 0)
+		{
+		        printf("Last digit of %d is %d and is 0\n", n, ldigit);
+				}
+	else if (ldigit < 6 && ldigit != 0)
+		{
+		         printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ldigit);
+				}
+       return (0);
 }
