85a07bef84ff858371ea134e3afcf869bd38a7b7
Helmeppo <gamerblliissard@gmail.com>
2022-10-19 16:46:07 -0700

    I'm tired already

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "_putchar.h"
 
 /**
  * main -  prints Holberton, followed by a new line
@@ -6,16 +6,14 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
