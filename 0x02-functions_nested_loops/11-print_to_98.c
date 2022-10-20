c08d2252b70357768ad67e6021a18fe711fc2d07
Helmeppo <gamerblliissard@gmail.com>
2022-10-19 22:59:19 -0700

    dfj

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,23 +9,24 @@
 void print_to_98(int n)
 {
 	int i, j;
+
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
 		{
 			if (i != 98)
-			printf("%d, ", i);
+				printf("%d, ", i);
 			else if (i == 98)
-			printf("%d\n", i);
+				printf("%d\n", i);
 		}
 	} else if (n >= 98)
 	{
 		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
-			printf("%d, ", j);
+				printf("%d, ", j);
 			else if (j == 98)
-			printf("%d\n", j);
+				printf("%d\n", j);
 		}
 	}
 }
