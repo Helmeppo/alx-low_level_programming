e700de12f15fc9d4993cd766feb06dfa73b97f0a
Helmeppo <gamerblliissard@gmail.com>
2022-10-19 17:31:02 -0700

    asd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
-#include "_putchar.c"
+#include "main.h"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main -  prints putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
