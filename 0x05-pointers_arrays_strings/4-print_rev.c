d6c920e105059ebe7feeb0bbc2b383202d48536a
Helmeppo <gamerblliissard@gmail.com>
2022-10-24 23:21:10 -0700

    Let's see

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#iinclude "main.h"
+/**
+ * print_rev - function
+ * @s: pointer of parameter
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	while (length > 0)
+		_putchar(s[--length]);
+	_putchar(10);
+}
