38eed710e3524df47be4685bad41abd473280e38
Helmeppo <113808268+Helmeppo@users.noreply.github.com>
2022-11-16 23:28:28 +0000

    After

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+* int_index - function that searches for an int
+* @array: array with parameters
+* @size: size of the array
+* @cmp: function to call function
+* Return: returns the index of the first element
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) == 1)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
