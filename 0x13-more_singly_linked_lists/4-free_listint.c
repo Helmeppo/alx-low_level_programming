9d2ecf99367bc38f21c1b763222bc192812eee91
Helmeppo <gamerblliissard@gmail.com>
2022-11-28 07:28:28 -0800

    lets

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+/**
+* free_listint -  function that frees a listint_t list.
+* @head: Parameter that point to the head of the linked list
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
